93aded38936ee3ee6b50484c5921365079f26924
hatumhussain <hatumhussain@gmail.com>
2026-03-09 19:53:39 +0500

    I am learning Java using Selenium

---
+++ LearningJavawithSelenium/src/JavaProject/FirstJavaProgram.java
@@ -0,0 +1,10 @@
+package JavaProject;
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hellow World Best Wishes This is my first program");
+
+	}
+
+}
