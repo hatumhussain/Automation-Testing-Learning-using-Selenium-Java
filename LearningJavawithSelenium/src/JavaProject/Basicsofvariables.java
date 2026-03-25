a2dc99b7eabe4824cc5cee36b7d34d7be1a07f5e
Hatum Hussain <61963956+hatumhussain@users.noreply.github.com>
2026-03-25 12:19:06 +0500

    Variables and Datatypes
    Learning Variables and Data Types using java programming.

---
+++ LearningJavawithSelenium/src/JavaProject/Basicsofvariables.java
@@ -0,0 +1,43 @@
+package datatypesandvariables;
+
+public class Basicsofvariables {
+
+	public static void main(String[] args) {
+		// Variable & DataTypes 
+		// Approach 1 
+		/* 
+		int a = 400;
+	    System.out.println(a);
+		
+	     a = 600;
+	    System.out.println(a);
+		*/
+	
+		/*
+		int a = 100;
+		int b = 200;
+		int c = 400;
+		*/
+		// Approach 2 
+		
+		/*
+		int a,b,c;
+		a = 100;
+		b = 300;
+		c = 200;
+		*/
+		
+		//Approach 3
+	    /*
+		int a=100; int b=200; int c=300;
+		System.out.println("The value of: " + a);
+		System.out.println("The value of: " + b);
+		System.out.println("The value of: " +c);
+		*/
+		
+		
+	}
+	
+	
+
+}
---
+++ LearningJavawithSelenium/src/JavaProject/variableanddatatypes.java
@@ -0,0 +1,49 @@
+package datatypesandvariables;
+
+public class variableanddatatypes {
+
+	public static void main(String[] args) {
+		//Numeric DataTypes 
+		int a=100 ,b=200;
+		System.out.println("The value of a is:" +a);
+		System.out.println("The value of b is:" +b);
+		System.out.println("The Sum of a & b is:"+(a+b));
+		
+		byte by=125;
+		System.out.println("The value of byte is:"+by);
+		
+		short sh = 32000;
+		System.out.println("The value of short is:"+sh);
+		
+		long lg=132132123L;
+		System.out.println("The value of long is:"+lg);
+		
+		
+		//Decimal numbers-float/double 
+		 float item_price=15.5f;
+		 System.out.println("The value of this:"+item_price);
+		 
+		 double dl=124.3343343434;
+		 System.out.println("The vlaue for dl is:"+dl);
+		
+		
+		//character Datatype
+		 char Grade='C';
+		 System.out.println("Your Grade is:"+Grade);
+		//Strings 
+		String Name="Hatum Hussain";
+		System.out.println(Name);
+		
+		//boolean
+		boolean qa = true;
+		System.out.println(qa);
+		 
+		 
+		 
+		
+		
+		
+		
+	}
+
+}
