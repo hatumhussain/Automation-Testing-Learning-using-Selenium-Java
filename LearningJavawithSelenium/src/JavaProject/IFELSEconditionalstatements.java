d0213416eff9c855ab83c3426c80b46cba5881c3
Hatum Hussain <61963956+hatumhussain@users.noreply.github.com>
2026-05-22 13:38:09 +0500

    if/Else Conditions

---
+++ LearningJavawithSelenium/src/JavaProject/IFELSEconditionalstatements.java
@@ -0,0 +1,113 @@
+package operatorsinJava;
+
+public class IFELSEconditionalstatements {
+
+	public static void main(String[] args) {
+		
+/*if  condition
+Question.01: Check the age for voting
+		int person_age=15;
+		if(person_age>=18)
+		{
+			System.out.println("Eligible to vote");
+		}
+		else
+		{
+			System.out.println("Not Eligible to vote");
+		}
+	*/
+/*Question 02: Checking the even and odd number?
+         int num =15;
+         if(num%2==0)
+         {
+        	 System.out.println("Even Number");
+         }
+         else
+         {
+        	 System.out.println("Odd Number");
+         } */
+//Question 03: Checking the number is positive or negative?
+      /* int num=-10;
+       if(num>0)
+       {
+    	   System.out.println("This is the Positive Number");
+       }
+       else if(num<0){
+    	   System.out.println("This is the Negative Number");
+       }
+       else
+       {
+    	   System.out.println("Zero");
+       }
+       */
+		/*
+//Question 04: Find the largest number 
+		int a=100,b=200,c=30;
+		if(a>b && a>c)
+		{
+			System.out.println("a is the largest number"  +a);
+		}
+		else if (b>a && b>c)
+		{
+			System.out.println("b is the largest number" +b);
+		}
+		else
+		{
+			System.out.println("c is the largest number"  +c);
+		}
+		*/
+//Question 05: Print the week months 
+		
+		int weekno =10;
+		if(weekno==1)
+		{
+			System.out.println("Sunday");
+		}
+		else if(weekno==2) {
+			System.out.println("Monday");
+		}
+		else if (weekno==3) {
+			System.out.println("Tuesday");
+		}
+		else if (weekno==4) {
+			System.out.println("Wednesday");
+		}
+		else if (weekno==5) {
+			System.out.println("Thursday");
+		}
+		else if (weekno==6) {
+			System.out.println("Friday");
+		}
+		else if (weekno==7) {
+			System.out.println("Saturday");
+		}
+		else {
+			System.out.println("No Record Found");
+		}
+		*/
+//Switch Cases - Question - Display the week names based on week number?
+		/*
+		int weekno =123;
+		switch(weekno)
+		{
+		case 1 : System.out.println("Sunday");
+		break;
+		case 2 : System.out.println("Monday");
+		break;
+		case 3: System.out.println("Tuesday");
+		break;
+		case 4: System.out.println("Wednesday");
+		break;
+		case 5: System.out.println("Thursday");
+		break;
+		case 6: System.out.println("Friday");
+		break;
+		case 7: System.out.println("Saturday");
+		break;
+		default: System.out.println("Invalid Week Number");
+		*/
+		}
+		
+	}
+	
+
