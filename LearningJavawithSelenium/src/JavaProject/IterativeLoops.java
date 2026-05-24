784f8754d054a331db197b286851821336d403dc
Hatum Hussain <61963956+hatumhussain@users.noreply.github.com>
2026-05-24 19:00:47 +0500

    Add files via upload
    Learning While Loop/Do While Loop/For Loop

---
+++ LearningJavawithSelenium/src/JavaProject/IterativeLoops.java
@@ -0,0 +1,29 @@
+package operatorsinJava;
+
+public class IterativeLoops {
+
+	public static void main(String[] args) {
+		//iterative modules
+		//While Loop
+		//Example : Print the Numbers 01 to 10
+	/*Ins i=0; //Initialization
+		while(i<=10) {            //condition
+			System.out.println(i);
+			i++;               //in
+	} */
+		
+// do while loop
+      /*int i=1;
+      do {
+    	  System.out.println(i);
+    	  i++;
+      }while(i<=10);
+		*/
+// For Loop 
+		for(int i=1; i<10; i++)
+		{
+			System.out.println(i);
+		}
+		
+}
+}
