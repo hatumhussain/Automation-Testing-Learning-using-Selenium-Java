65a8ec2b9e09602543cd81342335d0bf07419556
Hatum Hussain <61963956+hatumhussain@users.noreply.github.com>
2026-05-29 17:57:54 +0500

    Add files via upload
    More practice question for Arrays

---
+++ LearningJavawithSelenium/src/JavaProject/MoreexampleofanArrays.java
@@ -0,0 +1,39 @@
+package Arrays;
+
+public class MoreexampleofanArrays 
+{
+
+	public static void main(String[] args) 
+	{
+		//Example of an Two Dimensional and Single Dimentional Arrays
+		//Example 01: Search an Element with in array(linear search)
+		/*int a[] = {10,20,30,40,50};
+		int search_element=30;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]==search_element)
+			{
+				System.out.println("Element Found");
+				break;
+			}
+			else
+			{
+				System.out.println("Element Not Found");
+			}
+		}
+*/
+		//Example 02: Find how many duplicates in arrays
+		//int[] a = {100,300,100,400,100,600,100};
+		//int num = 100;
+		//int count=0;
+		//for(int value:a) 
+		//	{
+			//	if(value==num) {
+				//	count++;
+			//	}
+				
+			//}
+		//}
+//	}
+
+
